675f58b17498c24a5667d15a2e3f443664d7765d
DELL <rakeshdr543@gmail.com>
2021-01-14 16:23:42 +0530

    router redux added

--- src/Posts.js
+++ src/Posts.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {Route, Switch, NavLink, BrowserRouter} from 'react-router-dom';
 
 const Posts =()=>{
-    return(
-        <h2>Posts page
-        <Link to='/todos/3'>Children route</Link>
-        </h2>
+
+    const app = useSelector((state)=>state.app)
+    const {count} = app
+    const dispatch = useDispatch()
+    const increment = () => {
+        dispatch({type:'INCREMENT_COUNT',payload:count})
+    };
+
+    const decrement = () => {
+        dispatch({type:'DECREMENT_COUNT',payload:count})
+
+    };
+    return (
+        <>
+            <p>{count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <hr />
+            <h2>Posts page
+                <Link to='/todos/3'>Children route</Link>
+            </h2>
+        </>
     )
 }
 export default Posts
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import {Provider} from 'react-redux'
 import App from './App';
 
-ReactDOM.hydrate(
-    <BrowserRouter>
+import configureStore from './store/configureStore';
+
+const store = configureStore( window.REDUX_STATE || {} );
+
+const AppBundle = (
+<BrowserRouter>
+    <Provider store={store}>
         <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+    </Provider>
+</BrowserRouter>
 );
+
+window.onload = () => {
+    ReactDOM.hydrate(
+        AppBundle,
+        document.getElementById('root')
+    );
+};
